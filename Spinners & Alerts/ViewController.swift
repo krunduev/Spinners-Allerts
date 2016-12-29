72dcc736f0c5e9e5a01554758c7797eb0ea77cac
Kostyantyn Runduyev <krunduev@curiousit.eu>
2016-12-29 19:23:27 +0000

    Update

--- Spinners & Alerts/ViewController.swift
+++ Spinners & Alerts/ViewController.swift
@@ -9,7 +9,42 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var activityIndicator = UIActivityIndicatorView()
 
+    @IBAction func createAlert(_ sender: Any) {
+        let alertController = UIAlertController(title: "Hey There!", message: "Are you shure?", preferredStyle: UIAlertControllerStyle.alert)
+        
+        alertController.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.default, handler: { (action) in
+            print("OK")
+            self.dismiss(animated: true, completion: nil)
+        }))
+        
+        alertController.addAction(UIAlertAction(title: "NO", style: UIAlertActionStyle.default, handler: { (action) in
+            print("No")
+            self.dismiss(animated: true, completion: nil)
+        }))
+        
+        self.present(alertController, animated: true, completion: nil)
+        
+    }
+    
+    @IBAction func pauseApp(_ sender: Any) {
+        activityIndicator = UIActivityIndicatorView(frame: CGRect(x: 0, y: 0, width: 50, height: 50))
+        activityIndicator.center = self.view.center
+        activityIndicator.hidesWhenStopped = true
+        activityIndicator.activityIndicatorViewStyle = UIActivityIndicatorViewStyle.gray
+        view.addSubview(activityIndicator)
+        activityIndicator.startAnimating()
+//        UIApplication.shared.beginIgnoringInteractionEvents()
+    }
+    
+    @IBAction func restoreApp(_ sender: Any) {
+        activityIndicator.stopAnimating()
+//        UIApplication.shared.endIgnoringInteractionEvents()
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
